4601b5f847616ad408672a3708145a23b0b9d04f
Manigandan Ramadasan <72668299+NotManigandan@users.noreply.github.com>
2022-07-03 15:49:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ if (isset($_POST['loginbutton'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRMS</title>
-    <link rel="stylesheet" type="text/css" href="css/login.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
     <script type="text/javascript">
         function preventBack() {
             window.history.forward();
